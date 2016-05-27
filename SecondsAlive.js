30960dc0192de2f4089a00dc6562fac255c8e2bd
Chris Gavin <kinglazybones@gmail.com>
2016-05-27 10:23:13 -0400

    Add files via upload

---
+++ SecondsAlive.js
@@ -0,0 +1,11 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write('There are ' + secondsPerDay + ' seconds in a day');
+var yearsAlive = 3;
+var secondsPerYear = secondsPerDay * daysPerWeek * weeksPerYear;
+secondsAlive = secondsPerYear * yearsAlive;
+document.write("I've been alive for more than " + secondsAlive + " seconds!");
